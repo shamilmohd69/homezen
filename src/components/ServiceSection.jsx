f804ae836723f3e887ee019189f7a7fe0d0b05f4
unknown <shamilmohdpc@gmail.com>
2025-02-15 13:44:16 +0530

    Updated features and fixed bugs

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -9,7 +9,7 @@ const ServiceSection = forwardRef((props, ref) => {
 
             <h1 className='text-2xl sm:text-2xl lg:text-3xl font-bold text-gray-950 mt-5 text-center'>Our Services</h1>
 
-            <div className="grid gap-8 md:grid-cols-2 ">
+            <div className="grid gap-8 md:grid-cols-2 mt-10">
                 <div className="transform transition-transform hover:scale-[1.03] border border-gray-300 rounded-md cursor-pointer">
                     <div className="flex flex-col items-center p-6">
                         <Fan className="mb-4 h-12 w-12 text-[#125a77]" />
