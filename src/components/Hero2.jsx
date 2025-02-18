773e9a62142abe00888a70821019f57590089e7b
unknown <shamilmohdpc@gmail.com>
2025-02-18 13:10:48 +0530

    new design 4

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -14,14 +14,16 @@ const Hero2 = () => {
 
   return (
     <div className="h-screen flex flex-col sm:flex-row sm:justify-between sm:items-center gap-3 md:pl-10 mt-[65px] sm:mt-0 pt-20 md:pt-0 relative">
-      {/* Text Section */}
+      
       <div className={`text-black px-3 md:max-w-[50%] z-10 ${spaceGrotesk.className}`}>
+
         <h1 className="text-3xl md:text-5xl font-[500]">
           Expert Building Construction & AC Repair Services in Doha
         </h1>
         <p className="text-sm md:text-xl font-[300] mt-3">
           From foundation to finishing, we provide top-quality construction, renovation, AC installation, and efficient AC maintenance and repair services to ensure durability, comfort, and optimal performance for your home or business.
         </p>
+
         <div className="w-full flex flex-col md:flex-row mt-5 gap-2">
           <button className="bg-[#125a77] w-full text-white px-4 py-2 rounded-sm hover:bg-[#327691] transition duration-300">
             <Image
@@ -39,21 +41,19 @@ const Hero2 = () => {
         </div>
       </div>
 
-      {/* Image Section */}
       <div className="md:max-w-[50%] h-full w-full relative">
         <motion.div
           className="absolute inset-0"
-          initial={{ opacity: 0 }} // Start with opacity 0
-          animate={{ opacity: isImageLoaded ? 1 : 0 }} // Fade in once the image is loaded
-          transition={{ duration: 0.5, ease: "easeInOut" }} // Smooth transition
+          initial={{ opacity: 0 }} 
+          animate={{ opacity: isImageLoaded ? 1 : 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }} 
         >
           <Image
             src="/dohaImage.jpg"
             alt="Doha Image"
-            layout="fill" // Makes image cover the div
-            objectFit="cover" // Ensures the image covers without distortion
-            className="absolute inset-0"
-            onLoadingComplete={() => setIsImageLoaded(true)} // Set image loaded state
+            fill 
+            className="absolute inset-0 object-cover"
+            onLoad={() => setIsImageLoaded(true)}
           />
         </motion.div>
       </div>
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -15,13 +15,13 @@ const Service = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // Check if screen width is less than 768px
+      setIsMobile(window.innerWidth < 768);
     };
 
-    handleResize(); // Initialize on load
-    window.addEventListener("resize", handleResize); // Update on resize
+    handleResize();
+    window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize); // Clean up on unmount
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
@@ -30,14 +30,14 @@ const Service = () => {
         {/* Card 1 */}
         <motion.div
           className="w-full md:w-1/4 text-center flex flex-col items-center"
-          initial={{ opacity: 0, y: 50 }} // Start from bottom
-          whileInView={{ opacity: 1, y: 0 }} // Move to original position and fade in
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{
             duration: 0.8,
             ease: "easeOut",
-            delay: isMobile ? 0 : 0.0, // Remove delay for mobile devices
+            delay: isMobile ? 0 : 0.0,
           }}
-          viewport={{ once: true }} // Trigger animation once when in view
+          viewport={{ once: true }}
         >
           <Hammer className='h-10 w-10 mb-2' />
           <h3 className="text-2xl mb-2">Building Construction</h3>
@@ -49,14 +49,14 @@ const Service = () => {
         {/* Card 2 */}
         <motion.div
           className="w-full md:w-1/4 text-center flex flex-col items-center"
-          initial={{ opacity: 0, y: 50 }} // Start from bottom
-          whileInView={{ opacity: 1, y: 0 }} // Move to original position and fade in
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{
             duration: 0.8,
             ease: "easeOut",
-            delay: isMobile ? 0 : 0.2, // Remove delay for mobile devices
+            delay: isMobile ? 0 : 0.2,
           }}
-          viewport={{ once: true }} // Trigger animation once when in view
+          viewport={{ once: true }}
         >
           <Drill className='h-10 w-10 mb-2' />
           <h3 className="text-2xl mb-2">Building Renovation</h3>
@@ -68,14 +68,14 @@ const Service = () => {
         {/* Card 3 */}
         <motion.div
           className="w-full md:w-1/4 text-center flex flex-col items-center"
-          initial={{ opacity: 0, y: 50 }} // Start from bottom
-          whileInView={{ opacity: 1, y: 0 }} // Move to original position and fade in
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{
             duration: 0.8,
             ease: "easeOut",
-            delay: isMobile ? 0 : 0.4, // Remove delay for mobile devices
+            delay: isMobile ? 0 : 0.4,
           }}
-          viewport={{ once: true }} // Trigger animation once when in view
+          viewport={{ once: true }}
         >
           <Cog className='h-10 w-10 mb-2' />
           <h3 className="text-2xl mb-2">AC Installation & Setup</h3>
@@ -87,14 +87,14 @@ const Service = () => {
         {/* Card 4 */}
         <motion.div
           className="w-full md:w-1/4 text-center flex flex-col items-center"
-          initial={{ opacity: 0, y: 50 }} // Start from bottom
-          whileInView={{ opacity: 1, y: 0 }} // Move to original position and fade in
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{
             duration: 0.8,
             ease: "easeOut",
-            delay: isMobile ? 0 : 0.6, // Remove delay for mobile devices
+            delay: isMobile ? 0 : 0.6,
           }}
-          viewport={{ once: true }} // Trigger animation once when in view
+          viewport={{ once: true }}
         >
           <Wrench className='h-10 w-10 mb-2' />
           <h3 className="text-2xl mb-2">AC Repair & Maintenance</h3>
@@ -104,15 +104,65 @@ const Service = () => {
         </motion.div>
       </div>
 
-      {/* Image */}
-      <div className="h-full w-full relative">
-        <Image
-          src="/service.jpg"
-          alt="logo"
-          width={1000}
-          height={1000}
+      <div className="flex flex-col sm:flex-row sm:justify-between">
+        <div className="relative w-full sm:w-1/2 h-64 sm:h-[400px]">
+          <Image
+            src="/caro3.jpg"
+            alt="Doha Image"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+
+        <div className="w-full sm:w-1/2 p-4 flex flex-col gap-3 justify-center items-center px-5 sm:px-10">
+          <h3 className='text-3xl '>Your Partner for Cool Comfort in Doha</h3>
+          <p className='text-lg'>At Homezen Trading and Contracting, we specialize in providing top-notch AC maintenance and repair services across Doha, Qatar. Our dedicated team of professionals ensures that your cooling systems run efficiently, keeping your environment comfortable year-round. We pride ourselves on our commitment to quality service and customer satisfaction, making us the trusted choice for all your air conditioning needs.</p>
+          <div className='flex gap-5 items-center'>
+            <Image
+              src='/ac_carrier.png'
+              alt='carrier'
+              width={70}
+              height={70}
+            />
+            <Image
+              src='/ac_daikin.png'
+              alt='daikin'
+              width={70}
+              height={70}
+            />
+            <Image
+              src='/ac_lg.png'
+              alt='carrier'
+              width={50}
+              height={50}
+            />
+            <Image
+              src='/ac_samsung.png'
+              alt='carrier'
+              width={80}
+              height={80}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="w-full h-60 md:h-96 cursor-pointer relative"
+        onClick={() => window.open("https://www.google.com/maps/place/Doha", "_blank")}
+      >
+        <iframe
+          title=""
+          width="100%"
+          height="100%"
+          loading="lazy"
+          allowFullScreen
+          className="absolute inset-0 w-full h-full"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115666.27827319023!2d51.4196373707179!3d25.285632882459244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45dad68a8ff1b7%3A0xd6a51a7f45f4a5cf!2sDoha%2C%20Qatar!5e0!3m2!1sen!2sqa!4v1708173204047!5m2!1sen!2sqa&zoom=12&disableDefaultUI=true"
         />
       </div>
+
+
     </div>
   );
 };
